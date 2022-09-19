5a58ab6eb9974dd35e9eb0c628c6100b0e9c09c9
veceloe <81566766+veceloe@users.noreply.github.com>
2022-09-19 08:04:08 +0300

    New frendly-UI

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.png';
+import logo from './lookind.png';
+import person from './person.png';
 import './App.css';
 
 function App() {
     return (
+    <div id="Main">
         <div className="header">
             <div className="header-kat">
-                <div className="header-logo">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </div>
+                <img src={logo} className="App-logo" alt="logo" height="11%" width="11%"/>
                 <a className="kat" href="@">Главная</a>
                 <a className="kat" href="@">Каталог</a>
-                <a className="kat" href="@">Lookind.smart</a>
+                <a className="kat" href="@">Lookind.AI</a>
                 <a className="kat" href="@">О нас</a>
-                <div className="lk">
-                    <a className="button" href="#">Личный кабинет</a>
-                </div>
+                <a className="lkarea" href="@"><img id="account" src={person} width="15%" height="10%"/><p>Личный кабинет</p></a>
             </div>
         </div>
+    </div>
     );
 }
 

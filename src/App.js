f90547fb3d774e1ecb1899f032bf9a729405b219
veceloe <81566766+veceloe@users.noreply.github.com>
2022-09-20 13:49:11 +0300

    New frendly-UI c ЛК и списком

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import './App.css';
 
 function App() {
     return (
-    <div id="Main">
-        <div className="header">
-            <div className="header-kat">
-                <img src={logo} className="App-logo" alt="logo" height="11%" width="11%"/>
-                <a className="kat" href="@">Главная</a>
-                <a className="kat" href="@">Каталог</a>
-                <a className="kat" href="@">Lookind.AI</a>
-                <a className="kat" href="@">О нас</a>
-                <a className="lkarea" href="@"><img id="account" src={person} width="15%" height="10%"/><p>Личный кабинет</p></a>
+        <div id="Main">
+            <div className="header">
+                <div className="header-kat">
+                    <img src={logo} className="App-logo" alt="logo" height="11%" width="11%"/>
+                    <a className="kat" href="@">Главная</a>
+                    <div className="dropdown">
+                        <a className="kat">Каталог</a>
+                        <div className="dropdown-content">
+                            <a href="#">Для него</a>
+                            <a href="#">Для неё</a>
+                        </div>
+                    </div>
+                    <a className="kat" href="@">Lookind.AI</a>
+                    <a className="kat" href="@">О нас</a>
+                    <a className='lkramka' href="@" >
+                        <img id="account" src={person} width="15%" height="10%" alt="person"/>
+                        <p className="lkarea">Личный кабинет</p>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
     );
 }
 

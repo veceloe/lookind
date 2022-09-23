7fe60ce1a26905da3eaa102ab2c213d88646b70b
kazakovstep <kazakov.step@yandex.ru>
2022-09-24 01:56:46 +0300

    remake header

--- src/App.js
+++ src/App.js
@@ -5,25 +5,24 @@ import './App.css';
 function App() {
     return (
         <div id="Main">
-            <div className="header">
-                <div className="header-kat">
-                    <img src={logo} className="App-logo" alt="logo" height="11%" width="11%"/>
-                    <a className="kat" href="@">Главная</a>
-                    <div className="dropdown">
-                        <a className="kat">Каталог</a>
-                        <div className="dropdown-content">
-                            <a href="#">Для него</a>
-                            <a href="#">Для неё</a>
+                <div className="header">
+                      <a className="App-logo" href="@">Lookind</a>
+                        <div>
+                            <a className="kat">Каталог</a>
+                            <div className="dropdown-content">
+                                <a href="#">Для него</a>
+                                <a href="#">Для неё</a>
+                            </div>
                         </div>
-                    </div>
-                    <a className="kat" href="@">Lookind.AI</a>
-                    <a className="kat" href="@">О нас</a>
-                    <a className='lkramka' href="@" >
-                        <img id="account" src={person} width="15%" height="10%" alt="person"/>
-                        <p className="lkarea">Личный кабинет</p>
-                    </a>
+                        <a className="kat" href="@">Lookind.AI</a>
+                        <a className="kat" href="@">О нас</a>
+                        <a className='lkramka' href="@" >
+                            <div>
+                                <img id="account" src={person}  alt="person"/>
+                                <p className="lkarea">Личный кабинет</p>
+                            </div>
+                        </a>
                 </div>
-            </div>
         </div>
     );
 }

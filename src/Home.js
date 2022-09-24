c821a5932b57e0ebb337b1a6628e87d17a4ac0c8
Babidjon <gameproclass@gmail.com>
2022-09-25 02:07:27 +0300

    new content

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import front from "./photo.png";
-import post1 from "./1.jpg";
+import post1 from "./1.png";
+import post2 from './2.png'
+import post3 from './3.png'
+import post4 from './4.png'
 
 class Home extends Component {
     render() {
@@ -28,12 +31,36 @@ class Home extends Component {
                                 <p>Editor's picks</p>
                             </div>
                         </div>
-                        <div className="posts-wrappers">
+                        <div className="posts-wrapper">
                             <div className="post">
                                 <img src={post1} height="170px" alt="post1" className="post_img"/>
                                 <div className="post_content">
                                     <h4 className="post_title">Повседневный деловой стиль</h4>
                                     <p className="post_text">Стиль очень похож на smart casual, но правила сочетанияразных видов одежды намного строже...</p>
+                                    <a href="#" className="post_read-more">Read more</a>
+                                </div>
+                            </div>
+                            <div className="post">
+                                <img src={post2} height="170px" alt="post2" className="post_img"/>
+                                <div className="post_content">
+                                    <h4 className="post_title">Повседневный стиль</h4>
+                                    <p className="post_text">Этот стиль обычно сочетает в себе комфорт и индивидуальность, также подчеркивает элегантность образа...</p>
+                                    <a href="#" className="post_read-more">Read more</a>
+                                </div>
+                            </div>
+                            <div className="post">
+                                <img src={post3} height="170px" alt="post3" className="post_img"/>
+                                <div className="post_content">
+                                    <h4 className="post_title">Уличный стиль</h4>
+                                    <p className="post_text"> Уличный стиль одежды черпает вдохновение как из хип-хоп культуры, так и из стиля скейтеров...</p>
+                                    <a href="#" className="post_read-more">Read more</a>
+                                </div>
+                            </div>
+                            <div className="post">
+                                <img src={post4} height="170px" alt="post4" className="post_img"/>
+                                <div className="post_content">
+                                    <h4 className="post_title">Спортивный стиль</h4>
+                                    <p className="post_text">Спортивный стиль одежды выбирают не только для занятий в спорт-зале, но и для создания яркого сильного образа...</p>
                                     <a href="#" className="post_read-more">Read more</a>
                                 </div>
                             </div>
